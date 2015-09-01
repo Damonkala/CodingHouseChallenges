18a9b252a7abd77e204f234184b3bdd17266bdd7
Damon Anthony Egan Kalaboukis <damonkala@yahoo.com>
2015-09-01 16:03:22 -0700

    Update controller.js

--- Part-17-A-game-of-Hangman/controller.js
+++ Part-17-A-game-of-Hangman/controller.js
@@ -51,7 +51,7 @@ function titleScreen(){
 	
 function gameScreen(){
 	$('#gameContent').empty();
-	$('#gameContent').append('<div id="pixHolder"><img id="hangman" src="man.png"></div>');
+	$('#gameContent').append('<div id="pixHolder"><img id="hangman" src="stickman.png"></div>');
 	$('#gameContent').append('<div id="wordHolder"></div>');
 	$('#gameContent').append('<div id="clueHolder"></div>');
 	$('#gameContent').append('<div id="guesses">Previous guesses:</div>');
