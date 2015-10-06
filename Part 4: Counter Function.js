40decb9d9a8dfc7ab0f8a09784469a6e8a2de0d6
Damon Anthony Egan Kalaboukis <damonkala@yahoo.com>
2015-10-06 11:18:04 -0700

    Made it a return instead of several console.logs

--- Part 4: Counter Function.js
+++ Part 4: Counter Function.js
@@ -1,13 +1,12 @@
+
 function superCounter(text) {
     length = text.length;
     regex = /\s+/gi;
     wordCount = text.trim().replace(regex, ' ').split(' ').length;
     spaceCount = text.split(" ").length - 1;
     average = length / wordCount;
-  console.log(length + " characters");
-  console.log(wordCount + " words"); 
-  console.log(spaceCount + " spaces");
-  console.log(Math.floor(average) + " average words");
+    count = ("characters: " + length + ", " + "words: " + wordCount + ", " + "spaces: " + spaceCount + ", " + "average words " + Math.floor(average))
+    return count
+ 
 }
-superCounter("ex ample");
-//returns 8 characters 2 words 1 space and 4 average words
+superCounter("count me in");
